02dc8bb76a6fa3ac51086c6d67c675e4268e50e8
svalench <chitsalex@gmail.com>
2020-11-30 12:55:22 +0000

    запись данных в в  json

--- web/webserver.py
+++ web/webserver.py
@@ -65,7 +65,7 @@ def start_page():
                 a['netmask'] = address.netmask
                 a['broadcast'] = address.broadcast
                 addresses.append(a)
-    #partition_usage = psutil.disk_usage(partitions[0].mountpoint)
+    partition_usage = psutil.disk_usage(partitions[0].mountpoint)
     name = session['username']
     obj = {'username': name,
            'about': {'IP': IPAddr,
@@ -81,9 +81,9 @@ def start_page():
                      'mem_used': svmem.used,
                      'mem_total': svmem.total,
                      'mem_percent': svmem.percent,
-                     # 'part_used': partition_usage.used,
-                     # 'part_total': partition_usage.total,
-                     # 'part_procent': partition_usage.percent,
+                      'part_used': partition_usage.used,
+                      'part_total': partition_usage.total,
+                      'part_procent': partition_usage.percent,
                      'net': addresses,
                      'net_io': net_io,
                      }
@@ -205,8 +205,11 @@ def add_to_json(request):
     else:
         offset = request.form['bit']
     arr = []
+    try:
+        onchange_write = request.form['ifchange']
+    except:
+        onchange_write = 0
     if (request.form['type'] == 'area'):
-        print(request.form)
         for i in range(1, int(request.form['countVar']) + 1):
             tt = 'type_{}'.format(i)
             if (request.form[tt] == 'int'):
@@ -244,6 +247,7 @@ def add_to_json(request):
                                                           "DB": request.form['DB'],
                                                           "start": request.form['start'],
                                                           "offset": offset,
+                                                          'onchange':onchange_write,
                                                           "tablename": tablename,
                                                           'arr': arr})
     else:
