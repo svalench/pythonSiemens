6ff346acc6f70ba82694671e9e67130a60124962
svalench <chitsalex@gmail.com>
2020-11-30 12:28:33 +0000

    checkbox для записи данных по изменеию

--- secoundary_functions/mythread.py
+++ secoundary_functions/mythread.py
@@ -144,17 +144,14 @@ class MyThread(threading.Thread, metaclass=IterThread):
         print(tend - tstart)
 
     def _tread_for_write_data(self, c, data):
-        if 'DB_bind' in c:
-            if  c['tablename'] not in self.bind:
-                self.bind[c['tablename']] = BindError(data, c, self._plc1)
-            self.bind[c['tablename']].bind_error_function(data=data, c=c)
 
         try:
             value = self._plc1.transform_data_to_value(c['start'], c['offset'], data, c['type'])
             if 'DB_bind' in c:
-                # if not c['tablename'] in self.bind:
-                #     self.bind[c['tablename']] = BindError(data,c,self._plc1)
-                # self.bind[c['tablename']].bind_error_function(data,c)
+                if 'DB_bind' in c:
+                    if c['tablename'] not in self.bind:
+                        self.bind[c['tablename']] = BindError(data, c, self._plc1)
+                    self.bind[c['tablename']].bind_error_function(data=data, c=c)
                 self.__write_temp_value(c['tablename'], value)
             else:
                 self._write_value_to_db(c['tablename'], value)
@@ -246,7 +243,7 @@ class MyThread(threading.Thread, metaclass=IterThread):
             else:
                 cprint.info('data returned')
                 tend = datetime.datetime.now()
-                print('thread',tend-tstart)
+                print('thread', tend-tstart)
                 time.sleep(float(args[0]['timeout']))
 
 
@@ -262,6 +259,11 @@ class BindError:
         self.__accident_start_time = 0
         self.__accident_end_time = 0
         self.__accident_last = 0
+        self.__last_update = datetime.datetime.now()
+        self.deleay = 1
+        self.dleay_upd = self.deleay*2
+        self.step_upd = 1
+        self.__interval = 1
 
 
     def bind_error_function(self, data, c) -> None:
@@ -274,19 +276,45 @@ class BindError:
             self.__accident_temp = self.__accident
             if self.__accident_start_time == 0:
                 #  если событие происходит в первый раз то сохраняем с какого периода выбрать данные
-                self.__accident_start_time = datetime.datetime.now() - datetime.timedelta(minutes=1)
-                self.__accident_end_time = datetime.datetime.now() + datetime.timedelta(minutes=1)
+                self.__accident_start_time = datetime.datetime.now() - datetime.timedelta(minutes=self.deleay)
+                self.__accident_end_time = datetime.datetime.now() + datetime.timedelta(minutes=self.deleay)
             if self.__accident_last != self.__accident:
-                self.__accident_end_time = datetime.datetime.now() + datetime.timedelta(minutes=1)
+                self.__accident_end_time = datetime.datetime.now() + datetime.timedelta(minutes=self.deleay)
         self.__transfer_accident_data(self.c['tablename'])
 
     def __transfer_accident_data(self, tablename):
+        """Функция следит за данными если время пришло усредняет их или если произошла авария """
+        f = '%Y-%m-%d %H:%M:%S'
+        print(self.__last_update)
+        if (self.__accident_end_time == 0 and
+            self.__accident_start_time == 0 and
+            self.__accident_temp == 0 and
+        self.__last_update < datetime.datetime.now() - datetime.timedelta(minutes=self.dleay_upd)):
+            self._try_to_connect_db()
+            start_update = datetime.datetime.now() - datetime.timedelta(days=7)
+            end_update = datetime.datetime.now() - datetime.timedelta(minutes=self.dleay_upd)
+            start_update = start_update.strftime(f)
+            end_update = end_update.strftime(f)
+            sql = "WITH temp as (SELECT n as tt from generate_series('"+str(start_update)+"'::timestamp,'"+str(end_update)+"'::timestamp,'"+str(self.__interval)+" minute'::interval) n ) \
+INSERT  INTO  mvlab_"+str(tablename)+" (now_time, value) SELECT tt,(SELECT mode() WITHIN GROUP (ORDER BY value) as modevar FROM mvlab_temp_"+str(tablename)+" r WHERE  r.now_time>b.tt and r.now_time<=(b.tt+('"+str(self.__interval)+" minutes'::interval))) as value \
+from mvlab_temp_"+str(tablename)+" a LEFT JOIN temp b ON a.now_time>b.tt and a.now_time<=(b.tt+('"+str(self.__interval)+" minutes'::interval)) WHERE a.value IS NOT NULL GROUP BY tt ORDER BY tt asc;"
+            self._c.execute(sql)
+            self._conn.commit()
+            self._c.execute(
+                '''DELETE FROM mvlab_temp_''' + tablename + ''' WHERE
+                                         "now_time" >= %s AND 
+                                         "now_time" < %s  ;''', [start_update, end_update])
+            self._conn.commit()
+            sql_delete = "DELETE FROM mvlab_"+str(tablename)+" WHERE now_time>"+str(start_update)+" AND value is NULL;"
+            self._c.execute(sql_delete)
+            self._conn.commit()
+            self.__last_update = datetime.datetime.now()
+
         # если время вышло и была активна ошибка то переносим данные
         if (type(self.__accident_end_time)==type(datetime.datetime.now())
                 and self.__accident_end_time < datetime.datetime.now()
                 and self.__accident_temp==1):
             self._try_to_connect_db()
-            f = '%Y-%m-%d %H:%M:%S'
             totalsec_start = self.__accident_start_time.strftime(f)
             totalsec_end = self.__accident_end_time.strftime(f)
             self._c.execute(
